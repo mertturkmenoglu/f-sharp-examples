9c0941e6be5db267d84800ef1565b1909265ab5a
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 17:49:10 +0300

    Add function composition example

---
+++ f-sharp-examples/E012_FunctionComposition/Program.fs
@@ -0,0 +1,14 @@
+﻿let addThree n =
+    n + 3
+    
+    
+let addFive n =   
+   n + 5  
+  
+  
+[<EntryPoint>]
+let main argv =
+    let addEight = addThree >> addFive
+    let result = addEight 2
+    printfn "%d"result
+    0
