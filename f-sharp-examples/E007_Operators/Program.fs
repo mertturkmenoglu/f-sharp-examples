2d26dbdd3f19f376efad52d65c17b7be1991bfbf
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 14:31:38 +0300

    Add operator and if else ladder example

---
+++ f-sharp-examples/E007_Operators/Program.fs
@@ -0,0 +1,12 @@
+﻿let compare(a: int, b: int): string =
+    if (a < b) then
+        "<"
+    elif (a > b) then
+        ">"
+    else "="
+        
+[<EntryPoint>]
+let main argv =
+    printfn "Compare result: %s"(compare(3, 5))
+   
+    0
