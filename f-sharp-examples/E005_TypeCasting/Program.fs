23ddffc9675c7597b9198e294947cccf9f884ca7
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 14:11:22 +0300

    Add type casting example

---
+++ f-sharp-examples/E005_TypeCasting/Program.fs
@@ -0,0 +1,31 @@
+﻿type Animal() =
+    class
+        member this.PrintInfo() =
+            printfn "General animal"
+end
+
+
+type Squirrel() =
+    class
+        inherit Animal()
+        member this.PrintInfo() =
+            printfn "Little squirrel"
+end
+
+
+[<EntryPoint>]
+let main argv =
+    let a = new Animal()
+    let s: Squirrel = new Squirrel()
+    
+    a.PrintInfo()
+    s.PrintInfo()
+    
+    let castIntoAnimal = s :> Animal // Upcasting
+    castIntoAnimal.PrintInfo()
+    
+    // TODO: Unhandled exception
+//    let castIntoSquirrel = a :?>Squirrel    // Downcasting
+//    castIntoSquirrel.PrintInfo()
+    
+    0
