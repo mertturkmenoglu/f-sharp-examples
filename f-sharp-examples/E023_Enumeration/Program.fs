4d53cbfd84fad047692af28925317e8c7e5d9251
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 22:32:39 +0300

    Add enumeration example

---
+++ f-sharp-examples/E023_Enumeration/Program.fs
@@ -0,0 +1,12 @@
+﻿type Direction =  
+   | North = 0  
+   | West  = 1  
+   | South = 2  
+   | East  = 3  
+  
+let playerDirection = enum<Direction>(2)
+printfn "Player looks at %A" playerDirection
+
+let north: Direction = Direction.North
+let northValue = int north
+printfn "%A - %d" north northValue
