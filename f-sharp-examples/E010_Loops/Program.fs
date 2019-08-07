6ba13a4183462ae515d019d284d7e3c24228ba5f
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 17:30:42 +0300

    Add loop example

---
+++ f-sharp-examples/E010_Loops/Program.fs
@@ -0,0 +1,19 @@
+﻿[<EntryPoint>]
+let main argv =
+    for i = 0 to 5 do
+        printfn "%d"i
+        
+    for i = 5 downto 0 do
+        printfn "%d"i
+        
+    let names = ["emily"; "diana"; "barbara"]
+    
+    for name in names do
+        printfn "%s"name
+        
+    let mutable index = 0
+    while index < 5 do
+        printfn "%d"index
+        index <- index + 1
+        
+    0 
