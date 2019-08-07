edf21fcd9a8a29b2613d5540b21938200b9c2feb
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 17:53:12 +0300

    Add function pipelining example

---
+++ f-sharp-examples/E013_FunctionPipelining/Program.fs
@@ -0,0 +1,14 @@
+﻿let addThree n =
+    n + 3
+    
+    
+let addFive n =   
+   n + 5  
+
+
+[<EntryPoint>]
+let main argv =
+    let result = 2 |> addThree |> addFive
+    printfn "%d"result
+    
+    0
