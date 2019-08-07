f367d8c70675e66e4f2dd1a8e33205ba82f92985
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 17:19:19 +0300

    Add pattern matching example

---
+++ f-sharp-examples/E009_PatternMatching/Program.fs
@@ -0,0 +1,57 @@
+﻿// Pattern match
+let pmatch arg =
+    match arg with
+        | 1 -> "One"
+        | 2 -> "Two"
+        | _ -> "Other"
+
+
+// Object pattern match
+let opmatch(arg: obj) =
+    match arg with
+        | :? int -> "Integer"
+        | :? float -> "Real"
+        | _ -> "Other type"
+
+        
+// Conditional pattern match
+let cpmatch arg1 arg2 =
+    match (arg1, arg2) with
+        | (arg1, arg2) when arg1 > arg2 -> ">"
+        | (arg1, arg2) when arg1 < arg2 -> "<"
+        | _ -> "="
+
+
+// List pattern match
+let lpmatch list =
+    match list with
+        | [value] -> value
+        | [value1; value2] -> value1 + value2
+        | _ -> -1
+
+
+// Tuple pattern match
+let tpmatch tuple =
+    match tuple with
+        | ("a", "a") -> "AA"
+        | ("a", _) -> "A_"
+        | (_, "a") -> "_A"
+        | _ -> ""
+        
+        
+[<EntryPoint>]
+let main argv =
+    printfn "%s"(pmatch 2)
+    printfn "%s"(pmatch 42)
+    
+    printfn "%s"(opmatch(3.14))
+    printfn "%s"(opmatch("emily"))
+    
+    let result = cpmatch 3 5
+    printfn "%s"result
+    
+    printfn "%d"(lpmatch [1; 2])
+    
+    printfn "%s"(tpmatch ("a", "b"))
+    
+    0
