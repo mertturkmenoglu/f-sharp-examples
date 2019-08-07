b32414b70b4cf4254d0457b65336ce69a935d728
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 14:04:39 +0300

    Add conversion example

---
+++ f-sharp-examples/E004_Conversion/Program.fs
@@ -0,0 +1,12 @@
+﻿[<EntryPoint>]
+let main argv =
+    let x: int = 65
+    let y: byte = byte x
+    
+    printfn "y as int = %d"y
+    
+    let z: int = int y
+    
+    printfn "z as int = %d"z
+    
+    0
