5b17e25835229c216417413210a28395d60f454b
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 18:00:12 +0300

    Add inline function example

---
+++ f-sharp-examples/E015_InlineFunction/Program.fs
@@ -0,0 +1,9 @@
+﻿type MathUtil() =
+    class
+        member inline this.ReturnSquare(a: int) = a * a
+ end
+
+
+let mathUtil = new MathUtil()
+let result = mathUtil.ReturnSquare 3
+printfn "%d"result
