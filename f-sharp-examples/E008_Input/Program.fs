caa43f396dd49b9ddbb41afb283e0ff4905ce3c1
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 14:49:20 +0300

    Add user input example

---
+++ f-sharp-examples/E008_Input/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    Console.Write("Enter a number: ")
+    
+    let inp = Console.ReadLine()
+    let num = inp |> Int32.Parse
+    
+    if num > 0 then
+        printfn "Positive"
+    elif num = 0 then
+        printfn "Zero"
+    else printfn "Negative"
+    
+    0
