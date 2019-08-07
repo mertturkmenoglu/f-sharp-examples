562635199118225d561f934d4ff6d1a432f1bd1a
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 13:39:49 +0300

    Add function basics example

---
+++ f-sharp-examples/E003_FunctionBasics/Program.fs
@@ -0,0 +1,16 @@
+﻿(**
+  * Define a function
+  *)
+let addFunction x y = x + y
+
+[<EntryPoint>]
+let main arg =
+  printfn "Add 3 and 5"
+  printfn "%d"(addFunction 3 5)
+
+  let result = addFunction 3 5
+  printfn "%d"result
+
+  // Ignore return value
+  addFunction 3 5 |> ignore
+  0
