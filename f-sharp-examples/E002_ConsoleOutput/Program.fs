93130018373d05790531442984ac51b35facdf70
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 13:32:37 +0300

    Add console output example

---
+++ f-sharp-examples/E002_ConsoleOutput/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+printfn "Hello World with printfn"
+Console.WriteLine("Hello World with System.Console.WriteLine")
+
+let program =
+    printfn "Hello World with function declaration"
+    
+program
+
+type Program() =
+    class
+        do printfn "Hello World with class"
+    end
+    
+new Program() |> ignore
