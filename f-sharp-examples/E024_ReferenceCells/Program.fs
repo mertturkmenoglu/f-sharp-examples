51b40555ec544cf63ed553f5a9781de904357b5b
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 23:02:08 +0300

    Add reference cell example

---
+++ f-sharp-examples/E024_ReferenceCells/Program.fs
@@ -0,0 +1,17 @@
+﻿let refToFifty = ref 50
+let var = 40
+let reference = ref var
+
+printfn "%d" 50
+printfn "%d" refToFifty.Value
+printfn "%d" var
+printfn "%d" reference.Value
+
+reference := reference.Value + 1
+printfn "%d" reference.Value
+
+reference := !refToFifty
+printfn "%d" reference.Value
+
+reference.Value <- 42
+printfn "%d" reference.Value
