00d7cf697d3777f5587a5e8172bf99f8eb358694
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 23:06:18 +0300

    Add structure example

---
+++ f-sharp-examples/E025_Structure/Program.fs
@@ -0,0 +1,13 @@
+﻿type Student =
+    struct
+        val id: int
+        val name: string
+        val age: int
+        
+        // Constructor        
+        new (id, name, age) =
+            { id = id; name = name; age = age; }
+end
+
+let std = new Student(42, "Emily", 21)
+printfn "id: %d\nname: %s\nage: %d" std.id std.name std.age
