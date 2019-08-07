460f23a7d2a6aa446209690a3befabc3ae75c4d1
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 17:40:06 +0300

    Add recursive factorial function example

---
+++ f-sharp-examples/E011_FactorialFunction/Program.fs
@@ -0,0 +1,11 @@
+﻿let rec factorial n =
+    if n > 1 then
+        (n * factorial(n - 1))
+    else
+        1
+        
+        
+[<EntryPoint>]
+let main argv =
+    printfn "%d"(factorial 5)
+    0
