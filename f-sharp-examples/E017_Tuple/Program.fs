b71379bfee13ac6754aa0839846e8ea7c2341ee3
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 18:37:18 +0300

    Add Tuple example

---
+++ f-sharp-examples/E017_Tuple/Program.fs
@@ -0,0 +1,16 @@
+﻿let AddOneAndTwo n =
+    (n + 1, n + 2)
+
+let values = (3.14, 42, "Emily", (fun a -> a * a))  
+let (pi, answer, name, square) = values
+
+printfn "%A"values
+printfn "%f"pi
+printfn "%d"answer
+printfn "%s"name
+printfn "%d"(square 3)
+
+let vals = AddOneAndTwo 42
+printfn "%A"vals
+printfn "%d"(fst(vals))
+printfn "%d"(snd(vals))
