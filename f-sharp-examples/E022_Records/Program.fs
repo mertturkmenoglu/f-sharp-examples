9a7b2cd49393ac015eec627fb3ea80e9d1f88821
Mert <mertturkmenoglu99@gmail.com>
2019-08-07 22:28:12 +0300

    Add records example

---
+++ f-sharp-examples/E022_Records/Program.fs
@@ -0,0 +1,7 @@
+﻿type Vector2D = { x : float; y: float; }
+
+let unitVector = { x = 0.0; y = 1.0; }  
+
+printfn "Vector: %A" unitVector  
+printfn "Vector.x: %f" unitVector.x
+printfn "Vector.y: %f" unitVector.y
