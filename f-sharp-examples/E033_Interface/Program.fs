175c58d6584df2895c83b511d5996bff0d66b079
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:33:38 +0300

    Add interface example

---
+++ f-sharp-examples/E033_Interface/Program.fs
@@ -0,0 +1,11 @@
+﻿type IAnimal =
+    abstract member PrintInfo: unit -> unit
+    
+type Squirrel(name) =
+    interface IAnimal with
+        member this.PrintInfo() =
+            printfn "Squirrel: %s" name
+
+
+let alf = new Squirrel("alf")
+(alf :> IAnimal).PrintInfo()
