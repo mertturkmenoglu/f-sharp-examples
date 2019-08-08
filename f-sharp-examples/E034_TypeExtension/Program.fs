d772372fc2d3e7993cca1687e945f34334494e2f
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:36:34 +0300

    Add type extension example

---
+++ f-sharp-examples/E034_TypeExtension/Program.fs
@@ -0,0 +1,15 @@
+﻿type Squirrel() =
+    class
+        member this.Eat() =
+            printfn "Eating"
+    end
+
+
+type Squirrel with
+    member this.Sound() =
+        printfn "Making sound"
+
+
+let s = new Squirrel()
+s.Eat()
+s.Sound()
