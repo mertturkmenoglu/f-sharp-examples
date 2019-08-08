a6eab5fa7a5ab9ef9532797e55bb81c7c2b7b9dc
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:01:09 +0300

    Add class example

---
+++ f-sharp-examples/E029_Class/Program.fs
@@ -0,0 +1,20 @@
+﻿type Person(name, age) =
+    class
+        let name = name
+        let age = age
+        
+        member this.DisplayInfo() =
+            printfn "Person: %s - %d" name age
+    end
+    
+    
+type MathUtil() =
+    class
+        static member addOne(a) = a + 1
+    end
+    
+    
+let e = new Person("emily", 35)
+e.DisplayInfo()
+
+printfn "%d" (MathUtil.addOne 3)
