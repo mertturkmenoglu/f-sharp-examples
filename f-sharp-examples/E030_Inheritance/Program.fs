4066e2d5631aaf6cb3b0de2bceae98dff14b3cbb
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:05:42 +0300

    Add inheritance example

---
+++ f-sharp-examples/E030_Inheritance/Program.fs
@@ -0,0 +1,21 @@
+﻿type Animal(name) =
+    class
+        let name = name
+        
+        member this.PrintInfo =
+            printfn "Animal: %s" name
+    end
+    
+
+type Squirrel(name) =
+    class
+        inherit Animal(name)
+        
+        member this.PrintSquirrelInfo =
+            printfn "Squirrel: %s" name
+    end
+    
+    
+let alf = new Squirrel("alf")
+alf.PrintInfo
+alf.PrintSquirrelInfo
