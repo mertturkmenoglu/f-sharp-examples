262a3d295e124a7070e1cab0379bf5d3008eff12
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 12:26:55 +0300

    Add discriminated union example

---
+++ f-sharp-examples/E026_DiscriminatedUnion/Program.fs
@@ -0,0 +1,20 @@
+﻿type Squirrel = { name: string; color: string }
+
+type Mixed =  
+    | I of int  
+    | Sq of Squirrel  
+    | SpecialTuple of int * int  
+  
+let whatIsIt arg =  
+   match arg with  
+     | I _ -> "Integer"  
+     | Sq _ -> "Squirrel"       
+     | SpecialTuple (_, _) -> "Tuple"
+                           
+let alf = Sq { name = "alf"; color = "brown" }
+let number = I 42
+let myTuple = SpecialTuple (42, 42)
+  
+printfn "Alf is a %s" (whatIsIt alf)
+printfn "Number is an %s" (whatIsIt number)
+printfn "My tuple is a %s" (whatIsIt myTuple)
