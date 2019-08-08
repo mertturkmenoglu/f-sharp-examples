8cbe4e04686e14bf196887825881f338c805249c
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:30:26 +0300

    Add abstract class example

---
+++ f-sharp-examples/E032_AbstractClass/Program.fs
@@ -0,0 +1,17 @@
+﻿[<AbstractClass>]
+type Person() =
+    class
+        abstract member PrintName: unit -> unit
+    end
+    
+    
+type Emily() =
+    class
+        inherit Person()
+        override this.PrintName() =
+            printfn "Emily"
+    end
+    
+    
+let e = new Emily()
+e.PrintName()
