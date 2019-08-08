69c0c5ae4bcff6acf5c5bbdc039353d217cc1a04
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 15:08:51 +0300

    Add isogram word practice

---
+++ f-sharp-example/E001_IsogramWord/Program.fs
@@ -0,0 +1,15 @@
+﻿let isIsogram(str: string) =
+    let filtered =
+        str.ToLower().ToCharArray()
+        |> Array.filter (fun c -> c <> ' ' || c <> '-')
+        
+    let l =filtered |> Array.distinct
+    
+    l.Length = filtered.Length
+    
+[<EntryPoint>]
+let main args =
+    printfn "%b" (isIsogram("aBcD eFg-rM"))
+    printfn "%b" (isIsogram("mM"))
+    
+    0
