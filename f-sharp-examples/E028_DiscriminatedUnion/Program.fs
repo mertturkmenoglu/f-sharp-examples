47c202c987c4fd1b2b8343760778ce9fb77d13e9
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 13:21:40 +0300

    Add discriminated union example

---
+++ f-sharp-examples/E028_DiscriminatedUnion/Program.fs
@@ -0,0 +1,16 @@
+﻿type Light = | ON | OFF
+
+let lightSwitch = function
+    | ON -> OFF
+    | OFF -> ON
+    
+let main() =
+    let lamp = ON
+    printfn "%A" lamp
+    
+    let newStatus = lightSwitch(lamp)
+    printfn "%A" newStatus
+
+    0
+    
+main() |> ignore
