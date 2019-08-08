217f84a07febbf04939f79c5bc01593faa613440
Mert <mertturkmenoglu99@gmail.com>
2019-08-09 00:19:47 +0300

    Add even number sum practice

---
+++ f-sharp-examples/E002_EvenSum/Program.fs
@@ -0,0 +1,11 @@
+﻿let evenSum(list: List<int>): int =
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.sum
+
+let main args =
+    let result = evenSum [ 1; 1; 2; 4; 3; 9; 6; ]
+    printfn "%d" result
+    0
+
+main() |> ignore
