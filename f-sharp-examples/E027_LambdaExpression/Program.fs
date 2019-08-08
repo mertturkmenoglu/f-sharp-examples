70db8d670916247d3a041b0514258b192d9e9785
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 12:51:55 +0300

    Add lambda expression example

---
+++ f-sharp-examples/E027_LambdaExpression/Program.fs
@@ -0,0 +1,12 @@
+﻿let forEach(arr: int[], f:  (int -> unit)) =
+    for e in arr do
+        f(e)
+        
+let main args =
+    let arr = Array.init 5 (fun i -> i)
+    forEach(arr, (fun e -> printf "%d "e))
+    printfn ""
+    forEach(arr, (fun e -> printf "%.2f "(float(e) ** 2.0)))
+    0
+    
+main() |> ignore
