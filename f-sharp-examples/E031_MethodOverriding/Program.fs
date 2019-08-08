d0d53a1c7437331bef6f70bd588baa862dd3a5a1
Mert <mertturkmenoglu99@gmail.com>
2019-08-08 14:25:16 +0300

    Add method overriding example

---
+++ f-sharp-examples/E031_MethodOverriding/Program.fs
@@ -0,0 +1,21 @@
+﻿type BaseClass() =
+    class
+        abstract PrintClassName: unit -> unit
+        default this.PrintClassName() =
+            printfn "Base Class"
+    end
+    
+
+type DerivedClass() =
+    class
+        inherit BaseClass()
+        override this.PrintClassName() =
+            printfn "Derived Class"
+    end
+    
+    
+let b = new BaseClass()
+let d = new DerivedClass()
+
+b.PrintClassName()
+d.PrintClassName()
