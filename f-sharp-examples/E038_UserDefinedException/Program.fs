d911d26030858d489664d7e81e4c7073f42efc52
Mert <mertturkmenoglu99@gmail.com>
2019-08-09 13:17:08 +0300

    Add user defined exception example

---
+++ f-sharp-examples/E038_UserDefinedException/Program.fs
@@ -0,0 +1,13 @@
+﻿exception InvalidNameException of string
+let validateName name =
+    match name with
+        | "Emily" -> "Valid"
+        | _ -> raise(InvalidNameException "Not a valid name")
+
+let mutable result = ""
+try
+    result <- validateName "Emily"
+with
+    | :? InvalidNameException as e -> printfn "%s" e.Message
+    
+printfn "%s" result
