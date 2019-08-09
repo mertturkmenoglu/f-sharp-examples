46c87913cbef2c5b2bdf01ed7c67c3528c60ff5b
Mert <mertturkmenoglu99@gmail.com>
2019-08-09 13:10:54 +0300

    Add exception handling example

---
+++ f-sharp-examples/E037_ExceptionHandling/Program.fs
@@ -0,0 +1,13 @@
+﻿let division first second =
+    let mutable result = 0
+    try
+        try
+            result <- first / second
+        with
+            | :? System.DivideByZeroException as e -> printfn "%s" e.Message
+    finally
+        printfn "Division"
+    result
+
+printfn "6 / 2 = %d" (division 6 2)
+printfn "6 / 0 = %d" (division 6 0)
