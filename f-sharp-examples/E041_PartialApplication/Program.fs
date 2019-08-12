540c4212904baa6b974194f5c1df74b858310047
Mert <mertturkmenoglu99@gmail.com>
2019-08-12 03:14:09 +0300

    Add partial application and function pipeline example

---
+++ f-sharp-examples/E041_PartialApplication/Program.fs
@@ -0,0 +1,10 @@
+﻿let addNumbers(first: int, second: int) =
+    first + second
+
+let addOne n = addNumbers(n, 1)
+let addTwo n = addNumbers(n, 2)
+let addThree = addTwo >> addOne
+
+let numbers = [1..10]
+let result = numbers |> Seq.map addThree
+printfn "%A" result
