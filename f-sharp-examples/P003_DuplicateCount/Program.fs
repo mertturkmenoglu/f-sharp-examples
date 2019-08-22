62552d161e2a69d4947f5ff8e8d6cecd1ed020c2
Mert <mertturkmenoglu99@gmail.com>
2019-08-22 17:06:16 +0300

    Add duplicate count practice

---
+++ f-sharp-examples/P003_DuplicateCount/Program.fs
@@ -0,0 +1,11 @@
+﻿let duplicateCount(str: string) =
+    str.ToLower()
+    |> Seq.toList
+    |> List.groupBy(fun x -> x)
+    |> List.filter(fun x -> (snd x).Length > 1)
+    |> List.map(fun x -> (fst x, snd(x).Length))
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" ( duplicateCount "Canakkale")
+    0
