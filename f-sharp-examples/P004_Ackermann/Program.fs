f6202b44d2efffcaa34983a0769d4e6e41e892f2
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 21:33:28 +0300

    Add Ackermann implementation

---
+++ f-sharp-examples/P004_Ackermann/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec ackermann(m: int, n: int): int =
+    match (m, n) with
+        | (m, n) when m = 0 -> n + 1
+        | (m, n) when n = 0 -> ackermann(m - 1, 1)
+        | _ -> ackermann(m - 1, ackermann(m, n - 1))
+
+printfn "Ackermann(3, 5): %d" (ackermann(3, 5))
