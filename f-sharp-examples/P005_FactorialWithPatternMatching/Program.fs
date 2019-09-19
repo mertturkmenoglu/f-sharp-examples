49d78a43719e916edc7a9009965540f016e78eb8
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 21:40:36 +0300

    Add factorial practice

---
+++ f-sharp-examples/P005_FactorialWithPatternMatching/Program.fs
@@ -0,0 +1,8 @@
+﻿let rec fact n =
+    match n with
+        | n when n <= 1 -> 1
+        | _ -> n * fact(n-1)
+
+printfn "0! --> %d" (fact 0)
+printfn "1! --> %d" (fact 1)
+printfn "5! --> %d" (fact 5)
