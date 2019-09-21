0b64b865dd98d338730e29d9dbdffd9eddc355ab
Mert <mertturkmenoglu99@gmail.com>
2019-09-21 13:26:21 +0300

    Add prime number practice

---
+++ f-sharp-examples/P007_IsPrime/Program.fs
@@ -0,0 +1,16 @@
+﻿type System.Int32 with
+    member this.IsPrime() = match this with
+                            | this when this < 2 -> false
+                            | this when this = 2 -> true
+                            | this when this % 2 = 0 -> false
+                            | this when seq { 3 .. int(sqrt(float this)) } |> Seq.exists(fun x -> this % x = 0) -> false
+                            | _ -> true
+
+[<EntryPoint>]
+let main argv =
+    let numbers = [0; 1; 2; 3; 4; 5; 7; 8; 13; 27; 49; 89; 97]
+    for n in numbers do
+        printf "Number: %d" n
+        printfn "\tIsPrime: %b" (n.IsPrime())
+
+    0
