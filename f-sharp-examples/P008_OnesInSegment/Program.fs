d6207af6e913941153be61763d1b5e96d5437016
Mert <mertturkmenoglu99@gmail.com>
2019-09-28 14:50:15 +0300

    Add binary string example

---
+++ f-sharp-examples/P008_OnesInSegment/Program.fs
@@ -0,0 +1,17 @@
+﻿let ones(n: int) = System.Convert.ToString(n, 2).ToCharArray()
+                   |> Array.countBy (fun x -> x = '1')
+                   |> Array.length
+
+let countOnes(start: int, last: int): int =
+    if (start < 1) then
+        -1
+    else
+        [start .. last]
+        |> List.map (fun x -> ones(x))
+        |> List.reduce (fun acc x -> acc + x)
+
+[<EntryPoint>]
+let main argv =
+    let result = countOnes(4, 7)
+    printfn "%d" result
+    0
