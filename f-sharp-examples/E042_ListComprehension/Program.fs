2bdf5e7da678571f195873ce8dfc0abde0a9c403
Mert <mertturkmenoglu99@gmail.com>
2019-10-02 23:23:11 +0300

    Add list comprehension example

---
+++ f-sharp-examples/E042_ListComprehension/Program.fs
@@ -0,0 +1,13 @@
+﻿[<EntryPoint>]
+let main argv =
+    let numbers = [ for i in 0 .. 10 -> (i, i+1) ]
+
+    for n in numbers do
+        printfn "%A" n
+
+    let evens = [ for i in 1 .. 10 -> if (i % 2 = 0) then i else 0 ]
+                |> List.filter (fun x -> x <> 0)
+
+    printfn "%A" evens
+
+    0
