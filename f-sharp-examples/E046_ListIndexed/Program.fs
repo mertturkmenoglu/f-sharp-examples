bbc0a24acfe39a58de20f3dd973c5a0c29880ef4
Mert <mertturkmenoglu99@gmail.com>
2019-10-05 13:13:37 +0300

    Add List example

---
+++ f-sharp-examples/E046_ListIndexed/Program.fs
@@ -0,0 +1,9 @@
+﻿[<EntryPoint>]
+let main argv =
+    let names = [ "Emily"; "Diana"; "Barbara"; "Selina" ]
+    
+    for e in List.indexed names do
+        printf "%d: " (fst e)
+        printfn "%s" (snd e)
+
+    0
