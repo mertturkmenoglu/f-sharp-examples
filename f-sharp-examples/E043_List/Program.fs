ced796ddab41ef8fd4e96b757c2cc540b65f4c9c
Mert <mertturkmenoglu99@gmail.com>
2019-10-05 12:47:22 +0300

    Add List example

---
+++ f-sharp-examples/E043_List/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let strList = [ "Emily"; "Diana"; "barbara"; "selina" ]
+    let intList = [ 1; 2; 3; 4; 5 ]
+    let fibList = [ 1; 1; 2; 3; 5 ]
+    let emptyList = List.empty
+    let appended = List.append emptyList [ 1.1; 2.0; 3.14; 4.2; 5.1 ]
+    let avg = List.average appended
+    let choose = List.choose (fun (e: string) -> match e with
+                                                 | e when Char.IsUpper e.[0] -> Some(e)
+                                                 | _ -> None) strList
+    
+    printfn "%A" strList
+    printfn "%A" intList
+    printfn "%A" fibList
+    printfn "%A" emptyList
+    printfn "%A" appended
+    printfn "%A" avg
+    printfn "%A" choose
+    0
