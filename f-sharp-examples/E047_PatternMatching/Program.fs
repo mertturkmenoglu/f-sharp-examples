29226322cd5716526d6384e804cfe280d5441f93
Mert <mertturkmenoglu99@gmail.com>
2019-10-08 00:48:12 +0300

    Add pattern matching example on discriminated union

---
+++ f-sharp-examples/E047_PatternMatching/Program.fs
@@ -0,0 +1,14 @@
+﻿type Season = Winter | Spring | Summer | Fall
+
+let SeasonInfo (s: Season) =
+    match s with
+    | Winter -> printfn "Winter is coming"
+    | Spring -> printfn "Spring is ok"
+    | Summer -> printfn "My sweet summer child"
+    | Fall -> printfn "Fall of the knights"
+
+[<EntryPoint>]
+let main argv =
+    SeasonInfo(Season.Winter)
+    SeasonInfo(Summer)
+    0
