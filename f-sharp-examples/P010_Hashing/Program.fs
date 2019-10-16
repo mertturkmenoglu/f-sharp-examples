d037b5d12973fec91a05ab3f6348f6eefe11dfb7
Mert <mertturkmenoglu99@gmail.com>
2019-10-16 22:16:41 +0300

    Add string hash function

---
+++ f-sharp-examples/P010_Hashing/Program.fs
@@ -0,0 +1,17 @@
+﻿let stringHash (str: string) =
+    let r = float 31
+    let m = 97
+    let entries = List.ofArray (str.ToCharArray())
+                |> List.map (fun x -> int x - int 'a' + 1)
+                |> List.indexed
+
+    let mutable result = 0
+    for entry in entries do
+        result <- result + (snd entry) * int(r ** float(fst entry)) 
+    result % m
+
+[<EntryPoint>]
+let main argv =
+    let l = [ "elma"; "male"; "lame" ]
+    printfn "%A" (List.map stringHash l)
+    0
